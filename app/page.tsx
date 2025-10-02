0660779df666b2fe9956430e8fa299aefcc0795a
Hansjason97 <hansadiogo@gmail.com>
2025-10-02 15:30:31 +0100

    working on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,64 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
+
+  const stats:{amount:number, title:string}[] = [
+    {
+      amount: 14,
+      title: "Pays représentés"
+    },
+    {
+      amount: 2500,
+      title: "Membres actifs"
+    },
+    {
+      amount: 50,
+      title: "Projets d'impact réalisés"
+    }
+  ];
+
   return (
-    <div></div>
+    <main>
+      {/**Hero */}
+      <section id="hero" className="box-container grid grid-cols-1 gap-7 lg:grid-cols-3 lg:gap-8 xl:gap-10">
+        <div className="flex flex-col gap-5 sm:gap-7 md:gap-8 col-span-1 lg:col-span-2">
+          <div className="flex flex-col gap-2">
+            <h1><span className="text-primary-400">{"Bâtissons ensemble "}</span>{"l'Afrique de Demain"}</h1>
+            <p className="text-text-gray subtitle-text">{"Un réseau panafricain qui rassemble, forme et promeut les femmes leaders et bâtisseuses pour un développement inclusif, durable et équitable."}</p>
+          </div>
+          <div className="flex flex-row gap-3 flex-wrap">
+            <Link href={"/"}><Button size={"lg"} variant={"primary"}>{"Rejoindre le réseau"}</Button></Link>
+            <Link href={"/"}><Button size={"lg"} variant={"outline"}>{"Découvrir nos projets"}</Button></Link>
+          </div>
+        </div>
+        <img src={"/images/hero.webp"} alt="hero" className="hidden lg:flex w-full max-w-lg h-auto aspect-[4/3] object-cover rounded-sm sm:rounded-md md:rounded-lg"/>
+      </section>
+      {/**Partners */}
+      <div className="w-full h-20 bg-gray-100 flex items-center justify-center">{"Partenaires"}</div>
+      {/**About us */}
+      <section id="about" className="box-container flex flex-col gap-7 lg:flex-row lg:justify-between">
+        <img src="/images/about.webp" alt="about" className="w-full max-w-lg h-auto aspect-video object-cover rounded-sm sm:rounded-md md:rounded-lg" />
+        <div className="w-full max-w-2xl flex flex-col gap-6">
+          <h2 className="text-secondary-600">{"Qui nous sommes"}</h2>
+          <p className="text-base md:text-lg">{"Le "}<strong>{"Réseau des Femmes Bâtisseuses de l'Afrique de Demain "}</strong>{"est une organisation panafricaine qui a pour mission de renforcer le leadership féminin, soutenir l’entrepreneuriat et promouvoir l’égalité des genres."}
+          <br/>
+          {"Présent dans plusieurs pays d’Afrique et dans la diaspora, le réseau offre un espace de collaboration, de solidarité et d’innovation sociale."}
+          </p>
+          <Link href={"/a-propos"}><Button>{"En savoir plus"}</Button></Link>
+        </div>
+      </section>
+      {/**Stats */}
+      <section id="statistics" className="px-7 w-full flex items-center justify-center">
+        <div className="max-w-5xl w-full grid grid-cols-1 divide-x-0 divide-y lg:divide-y-0 lg:divide-x lg:grid-cols-3 bg-white">
+          {stats.map((item, id)=>(
+            <div key={id} className="py-3 px-5 flex flex-col gap-1 items-center justify-center min-h-[160px]">
+              <span className="leading-[125%] tracking-[-2%] text-[28px] sm:text-[32px] md:text-[36px] lg:text-[40px] font-bold text-text-default">{item.amount}<span className="text-primary-400">{"+"}</span></span>
+              <p className="text-[16px] sm:text-[18px] lg:text-[20px] leading-[150%] text-text-gray font-medium">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border bg-white shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2 has-[>svg]:px-4",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-11 rounded-md px-6 has-[>svg]:px-4",
+        lg: "h-11 md:h-13 rounded-md px-5 has-[>svg]:px-4 text-base md:text-lg",
         icon: "size-10",
       },
     },
